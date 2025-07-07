338ef31b6e8717ae5ba31f5e32426f98e147a054
unknown <https://github.com/sanikachogale1804.com>
2025-07-07 10:29:07 +0530

    7-july-2025

--- src/main/java/com/example/Demo/TicketManagementSystemCogent_1/Controller/UserController.java
+++ src/main/java/com/example/Demo/TicketManagementSystemCogent_1/Controller/UserController.java
@@ -1,6 +1,8 @@
 package com.example.Demo.TicketManagementSystemCogent_1.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,10 +17,23 @@ public class UserController {
     @Autowired
     private UserService service;
 
+    @CrossOrigin(origins = {
+		    "http://localhost:3000","http://127.0.0.1:3000",
+		    "http://192.168.1.91:3000",
+		    "http://45.115.186.228:3000"
+	 	})
     @PostMapping("/register")
-    public User register(@RequestBody User user) {
-        return service.register(user);
+    public ResponseEntity<?> register(@RequestBody User user) {
+        try {
+            User saved = service.register(user);
+            return ResponseEntity.ok(saved);
+        } catch (Exception e) {
+            e.printStackTrace(); // Log full error in console
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                                 .body("Registration failed: " + e.getMessage());
+        }
     }
+
 
     @CrossOrigin(origins = {
 		    "http://localhost:3000","http://127.0.0.1:3000",
