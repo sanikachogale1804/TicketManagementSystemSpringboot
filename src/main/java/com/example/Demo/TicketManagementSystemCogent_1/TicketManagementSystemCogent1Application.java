d7e390702aa148038f5a870bbab380bfcad8c4c2
unknown <https://github.com/sanikachogale1804.com>
2025-11-11 12:03:10 +0530

    11-oct-2025

--- src/main/java/com/example/Demo/TicketManagementSystemCogent_1/TicketManagementSystemCogent1Application.java
+++ src/main/java/com/example/Demo/TicketManagementSystemCogent_1/TicketManagementSystemCogent1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class TicketManagementSystemCogent1Application extends SpringBootServletInitializer{
 	
 	 @Override
